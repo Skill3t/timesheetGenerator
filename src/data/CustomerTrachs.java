7f7167a38f4303569e506b1c06d260b7d437aeeb
Skill3t <lars.lengersdorf@gmail.com>
2017-02-10 17:12:48 +0100

    structure

---
+++ src/data/CustomerTrachs.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Lars
+ */
+public class CustomerTrachs {
+    private List customeritems = new ArrayList<TrackedTimeItem>() ;
+    private String customername;
+    
+    
+}
