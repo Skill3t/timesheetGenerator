451d8016e508d3188969c1e179796b7b8e2ba906
Skill3t <Lars@larss-mbp.fritz.box>
2017-02-16 20:33:31 +0100

    AllTracks customerTracks change to Hashmap Export via ApachePOI Tamplate chose

--- src/logic/ConvertData.java
+++ src/logic/ConvertData.java
@@ -10,6 +10,9 @@ import data.CustomerTrachs;
 import data.TrackedTimeItem;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -17,29 +20,45 @@ import java.util.Calendar;
  */
 public class ConvertData {
 
-    public ArrayList<String[]> convert() {
+    ArrayList<String> sheetnames = new ArrayList();
+
+    public ConvertData() {
         AllTracks instance = AllTracks.getInstance();
+        Set set = instance.getAllCustomers().entrySet();
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry) iterator.next();
+            CustomerTrachs cusomer = (CustomerTrachs) mentry.getValue();
+            sheetnames.add(cusomer.getCustomername());
+        }
+
+    }
+
+    public ArrayList<String> getSheetnames() {
+        return sheetnames;
+    }
+
+    public ArrayList<String[]> convert(String customername) {
+        AllTracks instance = AllTracks.getInstance();
+        CustomerTrachs ti = instance.getAllCustomers().get(customername);
         String[] retline;
         ArrayList<String[]> allreturn = new ArrayList();
 
-        for (CustomerTrachs ti : instance.getAllCustomers()) {
+        for (TrackedTimeItem tti : ti.getCustomeritems()) {
+            retline = new String[8];
 
-            for (TrackedTimeItem tti : ti.getCustomeritems()) {
-                retline = new String[8];
-
-                Calendar cal = Calendar.getInstance();
-                cal.setTime(tti.getStartTime());
-                retline[0] = "" + cal.get(Calendar.DAY_OF_MONTH) + "." + cal.get(Calendar.MONTH) + "." + cal.get(Calendar.YEAR);
-                retline[1] = "" + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
-                cal.setTime(tti.getEndTime());
-                retline[2] = "" + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
-                retline[3] = "";
-                retline[4] = "";
-                retline[5] = ti.getCustomername();
-                retline[6] = tti.getKindOfAction();
-                retline[7] = tti.getKommand();
-                allreturn.add(retline);
-            }
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(tti.getStartTime());
+            retline[0] = "" + cal.get(Calendar.DAY_OF_MONTH) + "." + cal.get(Calendar.MONTH) + "." + cal.get(Calendar.YEAR);
+            retline[1] = "" + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
+            cal.setTime(tti.getEndTime());
+            retline[2] = "" + cal.get(Calendar.HOUR_OF_DAY) + ":" + cal.get(Calendar.MINUTE);
+            retline[3] = "";
+            retline[4] = "";
+            retline[5] = ti.getCustomername();
+            retline[6] = tti.getKindOfAction();
+            retline[7] = tti.getKommand();
+            allreturn.add(retline);
         }
         return allreturn;
     }
